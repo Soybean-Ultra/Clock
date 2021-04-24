2fbe85291e1a8e0a3b94550c7d89378ca34a7627
Soybean-Ultra <71563393+Soybean-Ultra@users.noreply.github.com>
2021-04-24 10:58:29 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+
+
+
+function setup() {
+  createCanvas(800,400);
+  hr = hour();
+  mn = minute();
+  sc = second();
+  angleMode(DEGREES);
+  scAngle = map(sc,0,60,0,360)
+ 
+
+
+function draw() {
+  background(255,255,255);  
+  
+  push();
+  rotate(scAngle);
+  stroke(255,0,0);
+  strokeWeight(7);
+  line(0,0,100,0);
+  pop();
+}
+}
